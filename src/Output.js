8e7e92f67570c163c1047ddef93f6d6dd774cc81
SumedhThota <sumedharya569@gmail.com>
2024-07-21 17:41:55 -0400

    Text edits

--- src/Output.js
+++ src/Output.js
@@ -1,5 +1,5 @@
 export default function Output ({bill, tip}){
     return <div>
-        <h2>You pay ${bill + tip}(${bill} + ${tip})</h2>
+        <h2>You pay ${bill + tip} in total (${bill} + ${tip})</h2>
     </div>
 }
